55dcf6028b7f9a500aa35f96b45f1e80c49aed09
Mixazol <Michel.zalonis@gmail.com>
2023-08-07 12:45:57 +0300

    Version 0.1

---
+++ game1/guess_the_number.py
@@ -0,0 +1,14 @@
+import random
+print( "welcome to my first game!")
+thenumber = random.randint(1,50)
+print("I think of a random number from 1 to 50")
+print("Are you smart enough to guess it?")
+guess = 0
+while guess != thenumber:
+    guess = int (input ("Split it out: "))
+    if guess > thenumber:
+        print ("Oops! Your guessed number is bigger than the number I thought!")
+    if guess < thenumber:
+        print ("Oops! Your guessed number is smaller than the number I thought!")
+    if guess == thenumber:
+        print ("Congracts! You Guessed the number correctly!")
