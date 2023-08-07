6b716cf39d7770e78c2111ba10e1b147d179f807
Mixazol <Michel.zalonis@gmail.com>
2023-08-07 18:20:16 +0300

    Fixed some bugs

--- game1/GTN_Greek_edition.py
+++ game1/GTN_Greek_edition.py
@@ -1,16 +1,25 @@
 import random
-print( "Καλοσωρισατε στο πρωτο μου παιχνιδι!")
+print( "Καλωσορίσατε στο πρωτο μου παιχνιδι!")
 name = input("Γραψε το ονομα σου εδω: ")
-thenumber = random.randint(1,50)
-print("Θα σκεφτω εναν αριθμο απο το 1 εως το 50")
-print("Εισαι αρκετα εξυπνος για να τον βρεις?")
-tries = 0
-guess = 0
-while guess != thenumber:
-    tries = tries + 1
-    guess = int (input ("Γραψε τον αριθμο που σκεφτηκες εδω: "))
-    if guess > thenumber:
-        print ("ουπς! Ο αριθμος που μαντεψες ειναι μεγαλυτερος απ'αυτον που σκεφτηκα !")
-    if guess < thenumber:
-        print ("Ουπς! Ο αριθμος που μαντεψες ειναι μικροτερος απ'αυτον που σκεφτηκα !") 
-print ("Συγχαρητηρια ",name,"! Μαντεψες σωστα τον αριθμο που σκεφτηκα σε ",tries," προσπαθιες !")
+playing=True
+while playing:
+    thenumber = random.randint(1,50)
+    print("Θα σκεφτω εναν αριθμο απο το 1 εως το 50")
+    print("Εισαι αρκετα εξυπνος για να τον βρεις?")
+    tries = 0
+    guess = 0
+    while guess != thenumber:
+        tries = tries + 1
+        guess = int (input ("Γραψε τον αριθμο που σκεφτηκες εδω: "))
+        if guess == 2011:
+            print ("Αριθμος = ",thenumber)
+        elif guess > thenumber:
+            print ("ουπς! Ο αριθμος που μαντεψες ειναι μεγαλυτερος απ'αυτον που σκεφτηκα !")
+        elif guess < thenumber:
+            print ("Ουπς! Ο αριθμος που μαντεψες ειναι μικροτερος απ'αυτον που σκεφτηκα !") 
+    print ("Συγχαρητηρια ",name,"! Μαντεψες σωστα τον αριθμο που σκεφτηκα σε ",tries," προσπαθιες !") 
+    q=input("Πατα το Q για να βγεις απο το παιχνιδη Η πατα οτιδηποτε αλλο για να συνεχισεις: ")
+    if q=='Q':
+        playing=False
+    else:
+        playing=True      
--- game1/GTN_easy.py
+++ game1/GTN_easy.py
@@ -1,17 +1,26 @@
 import random
 print( "welcome to my first game!")
 name = input("Type yor name here: ")
-thenumber = random.randint(1,20)
-print("Hello ",name,",")
-print("I think of a random number from 1 to 50")
-print("Are you smart enough to guess it?")
-tries = 0
-guess = 0
-while guess != thenumber:
-    tries = tries + 1
-    guess = int (input ("Split it out: "))
-    if guess > thenumber:
-        print ("Oops! The number you guessed  is bigger than the number I thought!")
-    if guess < thenumber:
-        print ("Oops! The number you guessed  is smaller than the number I thought!") 
-print ("Congracts ",name,"! You Guessed the number correctly in ",tries," tries !")
+playing=True
+while playing:
+    thenumber = random.randint(1,20)
+    print("Hello ",name,",")
+    print("I think of a random number from 1 to 50")
+    print("Are you smart enough to guess it?")
+    tries = 0
+    guess = 0
+    while guess != thenumber:
+        tries = tries + 1
+        guess = int (input ("Split it out: "))
+        if guess == 2011:
+            print ("number = ",thenumber)
+        elif guess > thenumber:
+            print ("Oops! The number you guessed  is bigger than the number I thought!")
+        elif guess < thenumber:
+            print ("Oops! The number you guessed  is smaller than the number I thought!") 
+    print ("Congracts ",name,"! You Guessed the number correctly in ",tries," tries !")
+    q=input("Press Q to close the game OR press any to continue playing the game: ")
+    if q=='Q':
+        playing=False
+    else:
+        playing=True      
