ac1da09859bf608dddda109323e884dfd4e77ffc
Mixazol <Michel.zalonis@gmail.com>
2023-08-07 13:20:36 +0300

    tries counter added

--- game1/guess_the_number.py
+++ game1/guess_the_number.py
@@ -4,11 +4,13 @@ name = input("Type yor name here: ")
 thenumber = random.randint(1,50)
 print("I think of a random number from 1 to 50")
 print("Are you smart enough to guess it?")
+tries = 0
 guess = 0
 while guess != thenumber:
+    tries = tries + 1
     guess = int (input ("Split it out: "))
     if guess > thenumber:
         print ("Oops! Your guessed number is bigger than the number I thought!")
     if guess < thenumber:
         print ("Oops! Your guessed number is smaller than the number I thought!") 
-print ("Congracts ",name,"! You Guessed the number correctly!")
+print ("Congracts ",name,"! You Guessed the number correctly in ",tries," tries !")
