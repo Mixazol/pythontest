ab20d8a0fd3017c40f7ed0bc839ffd4f32fe4359
Mixazol <Michel.zalonis@gmail.com>
2023-08-07 13:07:45 +0300

    username update

--- game1/guess_the_number.py
+++ game1/guess_the_number.py
@@ -1,5 +1,6 @@
 import random
 print( "welcome to my first game!")
+name = input("Type yor name here: ")
 thenumber = random.randint(1,50)
 print("I think of a random number from 1 to 50")
 print("Are you smart enough to guess it?")
@@ -9,6 +10,5 @@ while guess != thenumber:
     if guess > thenumber:
         print ("Oops! Your guessed number is bigger than the number I thought!")
     if guess < thenumber:
-        print ("Oops! Your guessed number is smaller than the number I thought!")
-    if guess == thenumber:
-        print ("Congracts! You Guessed the number correctly!")
+        print ("Oops! Your guessed number is smaller than the number I thought!") 
+print ("Congracts ",name,"! You Guessed the number correctly!")
